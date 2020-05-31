36454d985cfdabba5bdcd7b78c2b347e4bd4cf1a
Sean Nilan <sean.nilan@gmail.com>
2020-05-31 19:04:51 -0400

    test

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const longitude = "-70.247750";
 
 window.onload = () => {
     return navigator.geolocation.getCurrentPosition(function(position) {
-
         renderModel(position.coords);
     });
 }
@@ -19,13 +18,17 @@ const renderModel = (location) => {
     let latitude = location.latitude;
     let longitude = location.longitude;
     alert("loaded", latitude, longitude);
+    alert("loaded", latitude, longitude);
 
-    let model = document.createElement('a-entity');
+
+    let model = document.createElement('a-sphere');
     model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-    model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
-    model.setAttribute('rotation', '0 180 0');
-    model.setAttribute('animation-mixer', '');
-    model.setAttribute('scale', '0.5 0.5 0.5');
+    model.setAttribute("color","#4CC3D9");
+    model.setAttribute("radius", "1.25");
+    //model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+    //model.setAttribute('rotation', '0 180 0');
+    //model.setAttribute('animation-mixer', '');
+    //model.setAttribute('scale', '0.1 0.1 0.1');
 
     model.addEventListener('loaded', () => {
         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
