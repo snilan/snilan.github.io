a81e081b5025acf61f836cfb86efb1bfe767ed14
Sean Nilan <sean.nilan@gmail.com>
2020-05-31 20:55:52 -0400

    test

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ AFRAME.registerComponent('cursor-listener', {
       var lastIndex = -1;
       var COLORS = ['red', 'green', 'blue'];
       console.log("made it here", this.el);
+      alert(`init ${this.el.id}`);
       this.el.addEventListener('click', function (evt) {
+        lastIndex = (lastIndex + 1) % COLORS.length;
+        this.setAttribute('material', 'color', COLORS[lastIndex]);
+        console.log('I was clicked at: ', evt.detail.intersection.point);
+        alert("I was clicked");
+      });
+      this.el.addEventListener('touchstart', function (evt) {
         lastIndex = (lastIndex + 1) % COLORS.length;
         this.setAttribute('material', 'color', COLORS[lastIndex]);
         console.log('I was clicked at: ', evt.detail.intersection.point);
